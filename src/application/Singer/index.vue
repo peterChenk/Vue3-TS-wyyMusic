ed9fbca77dcd40aa632e5e136e8f94f196b63bb9
chenkun <904706170@qq.com>
2020-10-29 17:50:50 +0800

    歌手详情页滚动效果

--- src/application/Singer/index.vue
+++ src/application/Singer/index.vue
@@ -6,7 +6,7 @@
     <div class="ImgWrapper" :style="{backgroundImage: 'url('+ artist.picUrl +')'}" ref="imageWrapper">
       <div class="filter"></div>
     </div>
-    <div class="CollectButton">
+    <div class="CollectButton" ref="collectButton">
       <i className="iconfont">&#xe62d;</i>
       <span className="text">收藏</span>
     </div>
@@ -28,6 +28,7 @@ import * as Types from '@/store/action-types'
 import { GlobalState } from '@/store';
 import Scroll from "@/baseUI/scroll/index.vue";
 import SongsList from '../SongsList/index.vue'
+import { HEADER_HEIGHT } from "@/api/config";
 export default defineComponent({
   components: {
     Header,
@@ -51,10 +52,6 @@ export default defineComponent({
       router.back()
     }
 
-    function handleScroll(pos: any) {
-      console.log('handleScroll', pos)
-    }
-
     const OFFSET = 5;
     let initialHeight = 0
 
@@ -73,6 +70,63 @@ export default defineComponent({
       songScroll.value.refresh()
     })
 
+    const collectButton = ref<null | HTMLElement>(null)
+    const header = ref<null | HTMLElement>(null)
+
+    function handleScroll(pos: any) {
+      console.log('handleScroll', pos)
+      const height = initialHeight
+      const newY = pos.y
+      const imageDOM = imageWrapper.value
+      const buttonDOM = collectButton.value
+      const headerDOM = header.value;
+      const layerDOM = layer.value;
+      const minScrollY = -(height - OFFSET) + HEADER_HEIGHT;
+
+      const percent = Math.abs(newY / height);
+      //在歌手页的布局中，歌单列表其实是没有自己的背景的，layerDOM其实是起一个遮罩的作用，给歌单内容提供白色背景
+      //因此在处理的过程中，随着内容的滚动，遮罩也跟着移动
+      const imageDOMStyle = imageDOM?.style
+      const layerDOMStyle = layerDOM?.style
+      const buttonDOMStyle = buttonDOM?.style
+      const headerDOMStyle = headerDOM?.style
+      if (newY > 0) {
+        //处理往下拉的情况,效果：图片放大，按钮跟着偏移
+        if (imageDOMStyle) imageDOMStyle["transform"] = `scale(${1 + percent})`;
+        if (buttonDOMStyle) buttonDOMStyle["transform"] = `translate3d(0, ${newY}px, 0)`;
+        if (layerDOMStyle) layerDOMStyle.top = `${height - OFFSET + newY}px`;
+      } else if (newY >= minScrollY) {
+        //往上滑动，但是还没超过Header部分
+        if (layerDOMStyle) {
+          layerDOMStyle.top = `${height - OFFSET - Math.abs(newY)}px`;
+          layerDOMStyle.zIndex = '1';
+        }
+        if(imageDOMStyle) {
+          imageDOMStyle.paddingTop = "75%";
+          imageDOMStyle.height = '0';
+          imageDOMStyle.zIndex = '-1';
+        }
+        if (buttonDOMStyle) {
+          buttonDOMStyle["transform"] = `translate3d(0, ${newY}px, 0)`;
+          buttonDOMStyle["opacity"] = `${1 - percent * 2}`;
+        }
+      } else if (newY < minScrollY) {
+        //往上滑动，但是超过Header部分
+        if(layerDOMStyle) {
+          layerDOMStyle.top = `${HEADER_HEIGHT - OFFSET}px`;
+          layerDOMStyle.zIndex = '1';
+        }
+        //防止溢出的歌单内容遮住Header
+        if(headerDOMStyle) headerDOMStyle.zIndex = '100';
+        //此时图片高度与Header一致
+        if (imageDOMStyle){
+          imageDOMStyle.height = `${HEADER_HEIGHT}px`;
+          imageDOMStyle.paddingTop = '0';
+          imageDOMStyle.zIndex = '99';
+        }
+      }
+    }
+
     return {
       setShowStatusFalse,
       artist,
@@ -81,7 +135,9 @@ export default defineComponent({
       imageWrapper,
       songScrollWrapper,
       layer,
-      songScroll
+      songScroll,
+      collectButton,
+      header
     }
   }
   
